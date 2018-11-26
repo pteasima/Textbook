b40c4a140ef46f49a270fc6855281bdb7a00fe23
Petr Sima <ptea.sima@gmail.com>
2018-11-26 18:21:26 +0100

    nested environments experiment

--- Textbook.playground/Pages/UsersList.xcplaygroundpage/Contents.swift
+++ Textbook.playground/Pages/UsersList.xcplaygroundpage/Contents.swift
@@ -1,5 +1,29 @@
 import UIKit
 import PlaygroundSupport
+protocol WorldEnvironment { }
+extension WorldEnvironment {
+    public subscript<Value>(keyPath keyPath: WritableKeyPath<Environment, Value>) -> Value {
+        get { return environment[keyPath: keyPath] }
+        set { environment[keyPath: keyPath] = newValue }
+    } 
+}
+public struct Chapter: WorldEnvironment {
+    public var loadUsers: () -> [User] = { [] }
+}
+
+protocol ChapterEnvironment: WorldEnvironment {}
+extension ChapterEnvironment {
+    public subscript<Value>(keyPath keyPath: WritableKeyPath<Chapter, Value>) -> Value {
+        get { return chapter[keyPath: keyPath] }
+        set { chapter[keyPath: keyPath] = newValue }
+    } 
+}
+var chapter = Chapter()
+struct Page: ChapterEnvironment {
+    var didSelectUser: (User) -> Void = { print($0) }
+    
+}
+var page = Page()
 
 public final class UsersListViewController: UITableViewController {
     public var users: [User] = [] {
@@ -13,6 +37,8 @@ public final class UsersListViewController: UITableViewController {
         return with(tableView.dequeueCell(for: indexPath), mut(\.textLabel!.text, users.map(String.init(describing:))[indexPath.row]))
     }
     public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let x = page[keyPath: \.loadUsers]()
+        let y = page[keyPath: \.flow]
         //environment.flow.goToDateDetail(.init(date: dates[indexPath.row]))
     }
 }
