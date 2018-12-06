55871348c4e976194a8be88ec76f1a509e0cb156
Petr Sima <ptea.sima@gmail.com>
2018-12-06 23:51:06 +0100

    wip before ReactiveCocoa

--- Math.playground/Pages/Profile.xcplaygroundpage/Contents.swift
+++ Math.playground/Pages/Profile.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+extension NSObject: ReactiveExtensionsProvider {}
 enum Grade: String {
     case one
     case two
@@ -13,21 +13,29 @@ struct State {
     var user: User
 }
 
-let nameLabel : Rendering<User, UILabel> = simpleLabel
-    .pullback(get(\User.name))
+let reactiveLabel = Rendering<Property<String>, UILabel> { text in
+    return with(UILabel()) { (label: inout UILabel) in
+        label.reactive[\.text] <~
+            MutableProperty("foo")
+    }
+} 
+
+let nameLabel = simpleLabel
     .map(concat(
         set(\.frame, CGRect(origin:.zero, size: CGSize(width: 100, height: 100))),
         set(\.backgroundColor, .white)
     ))
-let grade: Rendering<User, UILabel> = simpleLabel
-    .pullback(compose(get(\Grade.rawValue),get(\User.grade)))
+let grade = simpleLabel
     .map(concat(
         set(\.frame, CGRect(origin:.zero, size: CGSize(width: 100, height: 100))),
         set(\.backgroundColor, .white)
     ))
 
-let profile: Rendering<User, UIStackView> = stack([nameLabel, grade].map { $0.map { $0 }})
+let profile: Rendering<Property<User>, UIStackView> = stack([
+    nameLabel.pullback(get(\.value.name)),
+    grade.pullback(compose(get(\Grade.rawValue),get(\User.grade), get(\.value))),
+    ].map { $0.map { $0 }})
     .map(set(\.axis, .vertical))
 
 
-environment.setLiveView(profile.render(User(name: "me", grade: .one)))
+environment.setLiveView(profile.render(Property(initial: User(name: "me", grade: .one), then: SignalProducer.timer(interval: DispatchTimeInterval.seconds(1), on: QueueScheduler.main).map { _ in User(name: "him", grade: .two)})))
